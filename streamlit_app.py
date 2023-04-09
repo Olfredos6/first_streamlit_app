e4ef02f5744faf35bd5ac0795b5a61e4beab6e50
An algorithm must be seen to be believed." Donald Knuth <Alfred.nehemie@gmail.com>
2023-04-09 12:41:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit as S
 
 import logging
+import os
 
 
 S.title("My Parents New Heathy Diner")
 print(f""" Something to view 
   {dir(S)}
 """)
-logging.critical("alfred.nehemie@gmail.com")
+logging.critical(os.path)
+for p in os.scandir("."):
+    logging.critical(p)
