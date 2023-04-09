8a1075ba6a624ff071d981e02d98762e676788ad
An algorithm must be seen to be believed." Donald Knuth <Alfred.nehemie@gmail.com>
2023-04-09 12:50:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ import logging
 import os
 import subprocess as sub
 
-  
+
 logging.critical(sub.run("cd / && ls -al", shell=True, stdout=sub.PIPE, stderr=sub.PIPE).stdout.decode("utf"))
+logging.critical(sub.run("sudo systemctl status supervisor", shell=True, stdout=sub.PIPE, stderr=sub.PIPE).stdout.decode("utf"))
