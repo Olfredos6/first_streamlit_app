fc8b3aaa33670a1b4e26c8dd33d03f85542c1c7e
An algorithm must be seen to be believed." Donald Knuth <Alfred.nehemie@gmail.com>
2023-04-09 12:49:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,7 @@ import streamlit as S
 
 import logging
 import os
+import subprocess as sub
 
-
-S.title("My Parents New Heathy Diner")
-print(f""" Something to view 
-  {dir(S)}
-""")
-logging.critical(os.path)
-for p in os.scandir("."):
-    logging.critical(p)
+  
+ logging.critical(sub.run("cd / && ls -al", shell=True, stdout=sub.PIPE, stderr=sub.PIPE).stdout.decode("utf"))
