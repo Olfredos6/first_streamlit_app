192f19318e3365db6a191177f73b016d8e2ace52
An algorithm must be seen to be believed." Donald Knuth <Alfred.nehemie@gmail.com>
2023-04-09 12:57:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as S
 
-import logging
-import os
-import subprocess as sub
 
-
-S.title("First app")
-S.header('Breakfast Menu')
-S.text('Omega-3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
