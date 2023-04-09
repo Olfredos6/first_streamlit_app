58cf22d755ae3ed4c8f2d3a90c6e75dc6901b77e
An algorithm must be seen to be believed." Donald Knuth <Alfred.nehemie@gmail.com>
2023-04-09 12:56:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ import os
 import subprocess as sub
 
 
-logging.critical(sub.run("cd / && ls -al", shell=True, stdout=sub.PIPE, stderr=sub.PIPE).stdout.decode("utf"))
-logging.critical(sub.run("systemctl status supervisor", shell=True, stdout=sub.PIPE, stderr=sub.PIPE).stdout.decode("utf"))
-logging.critical(sub.run("top", shell=True, stdout=sub.PIPE, stderr=sub.PIPE).stdout.decode("utf"))
+S.title("First app")
+S.header('Breakfast Menu')
+S.text('Omega-3 & Blueberry Oatmeal')
